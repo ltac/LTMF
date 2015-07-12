c8264997407b2f99d852001847b43d2ebaadf4ea
GRUBES <eric.grubaugh@gmail.com>
2015-07-12 14:20:00 -0700

    Added a new script to move optional F3 components out of standard mission file; moved optional F3 components to new root directory optional/;

--- scripts/clean-f3.bat
+++ scripts/clean-f3.bat
@@ -1,25 +1,24 @@
 @echo off
 
 rem Set commonly used paths
-frameworkDir = "..\LTMF.Altis"
-f3Dir = "%frameworkDir%\f"
-optDir = "..\optional"
+SET frameworkDir=..\LTMF.Altis
+SET f3Dir=%frameworkDir%\f
+SET optDir=..\optional
+SET optFDir=%optDir%\f
+
+rem List of F3 directories to move to optional area
+SET dirsToMove=(EandEcheck authorisedCrew cache casualtiesCap dynamicViewDistance groupJoin mapClickTeleport nametag removeBody setAISkill simplewoundingsystem zeus)
 
 rem Validate that core folders exist
 IF NOT EXIST %frameworkDir% EXIT /B
 IF NOT EXIST %f3Dir% EXIT /B
 IF NOT EXIST %optDir% mkdir %optDir%
+IF NOT EXIST %optFDir% mkdir %optFDir%
 
 rem Move entire unused F3 directories
-IF EXIST %f3Dir%\EandEcheck MOVE %f3Dir%\EandEcheck %optDir%
-IF EXIST %f3Dir%\authorisedCrew MOVE %f3Dir%\authorisedCrew %optDir%
-IF EXIST %f3Dir%\cache MOVE %f3Dir%\cache %optDir%
-IF EXIST %f3Dir%\casualtiesCap MOVE %f3Dir%\casualtiesCap %optDir%
-IF EXIST %f3Dir%\mapClickTeleport MOVE %f3Dir%\mapClickTeleport %optDir%
-IF EXIST %f3Dir%\nametag MOVE %f3Dir%\nametag %optDir%
-IF EXIST %f3Dir%\removeBody MOVE %f3Dir%\removeBody %optDir%
-IF EXIST %f3Dir%\setAISkill MOVE %f3Dir%\setAISkill %optDir%
-IF EXIST %f3Dir%\simplewoundingsystem MOVE %f3Dir%\simplewoundingsystem %optDir%
-IF EXIST %f3Dir%\zeus MOVE %f3Dir%\zeus %optDir%
+rem FIXME For some reason this will not overwrite files,
+rem so you're forced to delete everything in the optional/f dir
+rem before running this script
+FOR %%i IN %dirsToMove% DO IF EXIST %f3Dir%\%%i MOVE /Y %f3Dir%\%%i %optFDir%
 
 rem Use powershell for text replacement in files?
