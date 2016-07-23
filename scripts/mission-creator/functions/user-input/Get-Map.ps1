d9d90ea29ec71d31ed5c809c55457671e2bfd962
Edward Billington-Cliff <yddebc@gmail.com>
2016-07-23 14:05:58 +0100

    Add Tanoa to mission creator script

--- scripts/mission-creator/functions/user-input/Get-Map.ps1
+++ scripts/mission-creator/functions/user-input/Get-Map.ps1
@@ -12,42 +12,44 @@ function Get-Map {
 
 	# Doing the hotkeys this way yields double entries on the actual
 	# selection menu (e.g. [0] [0] Altis). Not sure of a better way
-	$altis = New-Object System.Management.Automation.Host.ChoiceDescription "[&0] Altis", "Altis"
-	$stratis = New-Object System.Management.Automation.Host.ChoiceDescription "[&1] Stratis", "Stratis"
-	$sahrani = New-Object System.Management.Automation.Host.ChoiceDescription "[&2] Sahrani", "Sahrani"
-	$cherno = New-Object System.Management.Automation.Host.ChoiceDescription "[&3] Chernarus", "Chernarus"
-	$chernoS = New-Object System.Management.Automation.Host.ChoiceDescription "[&4] Chernarus (Summer)", "Chernarus (Summer)"
-	$caribou = New-Object System.Management.Automation.Host.ChoiceDescription "[&5] Caribou", "Caribou"
-	$thirsk = New-Object System.Management.Automation.Host.ChoiceDescription "[&6] Thirsk", "Thirsk"
-	$thirskW = New-Object System.Management.Automation.Host.ChoiceDescription "[&7] Thirsk Winter", "Thirsk Winter"
-	$taki = New-Object System.Management.Automation.Host.ChoiceDescription "[&8] Takistan", "Takistan"
-	$takiM = New-Object System.Management.Automation.Host.ChoiceDescription "[&9] Takistan Mountains", "Takistan Mountains"
-	$zarg = New-Object System.Management.Automation.Host.ChoiceDescription "[&A] Zargabad", "Zargabad"
-	$shapur = New-Object System.Management.Automation.Host.ChoiceDescription "[&B] Shapur", "Shapur"
-	$nam = New-Object System.Management.Automation.Host.ChoiceDescription "[&C] Vietnam", "Vietnam"
-	$fata = New-Object System.Management.Automation.Host.ChoiceDescription "[&D] PR FATA", "PR FATA"
-	$celle = New-Object System.Management.Automation.Host.ChoiceDescription "[&E] Celle 2", "Celle 2"
-	$helv = New-Object System.Management.Automation.Host.ChoiceDescription "[&F] Helvantis", "Helvantis"
-	$pant = New-Object System.Management.Automation.Host.ChoiceDescription "[&G] Panthera", "Panthera"
-	$im = New-Object System.Management.Automation.Host.ChoiceDescription "[&H] Imrali", "Imrali"
-	$nzi = New-Object System.Management.Automation.Host.ChoiceDescription "[&I] N'Ziwasogo", "N'Ziwasogo"
-	$kund = New-Object System.Management.Automation.Host.ChoiceDescription "[&J] Kunduz", "Kunduz"
-	$ukr = New-Object System.Management.Automation.Host.ChoiceDescription "[&K] Orshanetz", "Orshanetz"
-	$id = New-Object System.Management.Automation.Host.ChoiceDescription "[&L] Isla Duala", "Isla Duala"
-	$utes = New-Object System.Management.Automation.Host.ChoiceDescription "[&M] Utes", "Utes"
-	$bys = New-Object System.Management.Automation.Host.ChoiceDescription "[&N] Bystrica", "Bystrica"
-	$buk = New-Object System.Management.Automation.Host.ChoiceDescription "[&O] Bukovina", "Bukovina"
-	$porto = New-Object System.Management.Automation.Host.ChoiceDescription "[&P] Porto", "Porto"
-	$rahmadi = New-Object System.Management.Automation.Host.ChoiceDescription "[&Q] Rahmadi", "Rahmadi"
-	$fallujah = New-Object System.Management.Automation.Host.ChoiceDescription "[&R] Fallujah", "Fallujah"
-	$ryak = New-Object System.Management.Automation.Host.ChoiceDescription "[&S] Al Ryak", "Al Ryak"
-	$des = New-Object System.Management.Automation.Host.ChoiceDescription "[&T] Desert", "Desert"
-	$pg = New-Object System.Management.Automation.Host.ChoiceDescription "[&U] Proving Grounds", "Proving Grounds"
-	$vr = New-Object System.Management.Automation.Host.ChoiceDescription "[&V] Virtual Reality", "Virtual Reality"
+	$altis 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&0] Altis", 				"Altis"
+	$stratis 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&1] Stratis", 			"Stratis"
+	$tanoa 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&2] Tanoa", 				"Tanoa"
+	$sahrani 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&3] Sahrani", 			"Sahrani"
+	$cherno 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&4] Chernarus", 			"Chernarus"
+	$chernoS 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&5] Chernarus (Summer)", 	"Chernarus (Summer)"
+	$caribou 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&6] Caribou", 			"Caribou"
+	$thirsk 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&7] Thirsk", 				"Thirsk"
+	$thirskW 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&8] Thirsk Winter", 		"Thirsk Winter"
+	$taki 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&9] Takistan", 			"Takistan"
+	$takiM 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&A] Takistan Mountains", 	"Takistan Mountains"
+	$zarg 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&B] Zargabad", 			"Zargabad"
+	$shapur 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&C] Shapur", 				"Shapur"
+	$nam 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&D] Vietnam", 			"Vietnam"
+	$fata 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&E] PR FATA", 			"PR FATA"
+	$celle 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&F] Celle 2", 			"Celle 2"
+	$helv 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&G] Helvantis", 			"Helvantis"
+	$pant 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&H] Panthera", 			"Panthera"
+	$im 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&I] Imrali", 				"Imrali"
+	$nzi 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&J] N'Ziwasogo", 			"N'Ziwasogo"
+	$kund 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&K] Kunduz", 				"Kunduz"
+	$ukr 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&L] Orshanetz", 			"Orshanetz"
+	$id 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&M] Isla Duala", 			"Isla Duala"
+	$utes 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&N] Utes", 				"Utes"
+	$bys 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&O] Bystrica", 			"Bystrica"
+	$buk 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&P] Bukovina", 			"Bukovina"
+	$porto 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&Q] Porto", 				"Porto"
+	$rahmadi 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&R] Rahmadi", 			"Rahmadi"
+	$fallujah 	= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&S] Fallujah", 			"Fallujah"
+	$ryak 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&T] Al Ryak", 			"Al Ryak"
+	$des 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&U] Desert", 				"Desert"
+	$pg 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&V] Proving Grounds", 	"Proving Grounds"
+	$vr 		= New-Object System.Management.Automation.Host.ChoiceDescription 	"[&W] Virtual Reality", 	"Virtual Reality"
 
 	$options = [System.management.Automation.Host.ChoiceDescription[]](
 		$altis,
 		$stratis,
+		$tanoa,
 		$sahrani,
 		$cherno,
 		$chernoS,
@@ -73,8 +75,8 @@ function Get-Map {
 		$rahmadi,
 		$bys,
 		$buk,
-    $fallujah,
-    $ryak,
+    	$fallujah,
+    	$ryak,
 		$des,
 		$pg,
 		$vr
@@ -88,6 +90,7 @@ function Get-Map {
 	$folderNames = (
 		"Altis",
 		"Stratis",
+		"Tanoa",
 		"smd_sahrani_a3",
 		"Chernarus",
 		"Chernarus_Summer",
@@ -113,8 +116,8 @@ function Get-Map {
 		"Intro",
 		"Woodland_ACR",
 		"Bootcamp_ACR",
-    "fallujah",
-    "pja310",
+    	"fallujah",
+    	"pja310",
 		"Desert_E",
 		"ProvingGrounds_PMC",
 		"vr"
