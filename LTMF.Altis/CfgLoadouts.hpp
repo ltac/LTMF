1ee988bda4d1032e307ae95d1834109dc43b56d4
Edward Billington-Cliff <yddebc@gmail.com>
2016-09-15 20:19:22 +0100

    Switch default medical system to ACE advanced. Switch default Blufor loadout to US Army UCP. Sort loadouts into completed and WIP (i.e. non existent)

--- LTMF.Altis/CfgLoadouts.hpp
+++ LTMF.Altis/CfgLoadouts.hpp
@@ -11,8 +11,8 @@ class CfgLoadouts {
 
     /* BLUFOR FACTION */
 
-    #include "BW\Loadouts\blu_baf_mtp.hpp" // British Armed Forces
-    //#include "BW\Loadouts\blu_usarmy_ucp.hpp" // US Army, UCP, M4
+    //#include "BW\Loadouts\blu_baf_mtp.hpp" // British Armed Forces
+    #include "BW\Loadouts\blu_usarmy_ucp.hpp" // US Army, UCP, M4
     //#include "BW\Loadouts\blu_usarmy_ocp.hpp" // US Army, OCP, M4
     //#include "BW\Loadouts\blu_usmc_desert.hpp" // USMC, Desert MARPAT, M4
     //#include "BW\Loadouts\blu_usmc_woodland.hpp" // USMC, Woodland MARPAT, M4
@@ -20,21 +20,23 @@ class CfgLoadouts {
     //#include "BW\Loadouts\blu_germany_woodland.hpp" // Germany, Autumn Flecktarn, HK/G3
 
     /* INDFOR FACTION */
-    //#include "BW\Loadouts\ind_finland_snow.hpp" // WIP
-    //#include "BW\Loadouts\ind_militia_desert.hpp" // WIP
-    //#include "BW\Loadouts\ind_militia_woodland.hpp" // WIP
     //#include "BW\Loadouts\ind_pmc.hpp" // WIP
     #include "BW\Loadouts\ind_ukraine.hpp" // Ukraine, Woodland, AK47/74
     //#include "BW\Loadouts\ind_aaf.hpp" // AAF, Green Digi, F2000
+    //#include "BW\Loadouts\ind_militia_desert.hpp"
+
+    //#include "BW\Loadouts\ind_militia_woodland.hpp" // WIP
+    //#include "BW\Loadouts\ind_finland_snow.hpp" // WIP
 
     /* OPFOR FACTION */
+    #include "BW\Loadouts\opf_russia_woodland.hpp" // Russia, Woodland Digital, AK74
+    //#include "BW\Loadouts\opf_afghan.hpp" // Afghani, AKM
+
     //#include "BW\Loadouts\opf_csat_snow.hpp" // WIP
     //#include "BW\Loadouts\opf_pla_desert.hpp" // WIP
     //#include "BW\Loadouts\opf_pla_woodland.hpp" // WIP
     //#include "BW\Loadouts\opf_russia_desert.hpp" // WIP
-    #include "BW\Loadouts\opf_russia_woodland.hpp" // Russia, Woodland Digital, AK74
     //#include "BW\Loadouts\opf_russia_snow.hpp" // WIP
-    //#include "BW\Loadouts\opf_afghan.hpp" // Afghani, AKM
 
     /* CIVILIAN FACTIONS */
 	// Comment this out if you are using a different civilian faction
